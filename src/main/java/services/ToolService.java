6fef7eea9c8608ab2f03e98ec2169eb8e7961581
Eryk Kowalczyk <kowalczykeryk@op.pl>
2025-06-28 15:54:38 +0200

    added lists, constructor

--- src/main/java/services/ToolService.java
+++ src/main/java/services/ToolService.java
@@ -1,4 +1,15 @@
 package services;
 
+import models.Hire;
+import models.Tool;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ToolService {
+    List<Tool> tools = new ArrayList<>();
+    List<Hire> hires = new ArrayList<>();
+
+    public ToolService() {
+    }
 }

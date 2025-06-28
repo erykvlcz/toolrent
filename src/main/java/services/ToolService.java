443858bf1c6ff76c0759ecd0c9eef963c54e224a
Eryk Kowalczyk <kowalczykeryk@op.pl>
2025-06-28 16:51:57 +0200

    added addClient(), addTools(), showAvailableTools(), showOffer

--- src/main/java/services/ToolService.java
+++ src/main/java/services/ToolService.java
@@ -1,7 +1,6 @@
 package services;
 
-import models.Hire;
-import models.Tool;
+import models.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +9,7 @@ import java.util.Scanner;
 public class ToolService {
     List<Tool> tools = new ArrayList<>();
     List<Hire> hires = new ArrayList<>();
+    List<Client> clients = new ArrayList<>();
 
     public ToolService() {
     }
@@ -47,8 +47,9 @@ public class ToolService {
                 case 1 -> rent(scanner);
                 case 2 -> bringBack(scanner);
                 case 3 -> addClient(scanner);
-                case 4 -> showAvailableTools(scanner);
-                case 5 -> showOffer(scanner);
+                case 4 -> addTool(scanner);
+                case 5 -> showAvailableTools(scanner);
+                case 6 -> showOffer(scanner);
                 case 0 -> exitSystem();
             }
         }
@@ -61,12 +62,41 @@ public class ToolService {
     }
 
     private void addClient(Scanner scanner) {
+        System.out.println("Podaj id klienta: ");
+        String id = scanner.nextLine();
+        if(!id.equals("")){
+            clients.add(new Client(id));
+        }else {
+            System.out.println("Wartość nie może być pusta");
+        }
+
+    }
+
+    private void addTool(Scanner scanner) {
+        System.out.println("Podaj typ narzędzia: ");
+        String type = scanner.nextLine().trim().toLowerCase();
+        switch (type){
+            case "mlotek" -> tools.add(new Tool("młotek", TypeTool.HAMMER));
+            case "lopata" -> tools.add(new Tool("łopata", TypeTool.SHOVEL));
+            case "wiertarka" -> tools.add(new Tool("wiertarka", TypeTool.DRILL));
+            default -> System.out.println("Nie ma takiego narzędzia");
+        }
     }
 
     private void showAvailableTools(Scanner scanner) {
+        System.out.println("Dostępne narzędzia: ");
+        for (Tool tool : tools) {
+            if(tool.getRentStatus() == RentStatus.AVAILABLE){
+                System.out.println(tool);
+            }
+        }
     }
 
     private void showOffer(Scanner scanner) {
+        System.out.println("Oferta: ");
+        for (Tool tool : tools) {
+            System.out.println(tool);
+        }
     }
 
     private void exitSystem() {
